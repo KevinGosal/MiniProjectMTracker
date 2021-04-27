3fc70056e01a37662b00f32dbc4d7e691fec2d30
KevinGosal <s11810042@student.unklab.ac.id>
2021-04-28 03:21:23 +0800

    commit tambahan dari pertemuan 27

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import { SignIn, SplashScreen } from '../pages';
+import { SignIn, SplashScreen, SignUp } from '../pages';
 
 const Stack = createStackNavigator();
 const Router = () =>{
@@ -8,6 +8,7 @@ const Router = () =>{
         <Stack.Navigator>
             <Stack.Screen name="SplashScreen" component={SplashScreen}options={{headerShown: false}}/>
             <Stack.Screen name="SignIn" component={SignIn}options={{headerShown: false}}/>
+            <Stack.Screen name="SignUp" component={SignUp}options={{headerShown: false}}/>
         </Stack.Navigator>
     );
 };
